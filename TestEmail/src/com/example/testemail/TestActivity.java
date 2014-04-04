4ca3382b9a3ab3193f6815f52828df293395f00b
huanghui6579@163.com <huanghui6579@163.com>
2014-04-04 11:57:08 +0800

    添加了TestActivity

---
+++ TestEmail/src/com/example/testemail/TestActivity.java
@@ -0,0 +1,12 @@
+package com.example.testemail;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+}
